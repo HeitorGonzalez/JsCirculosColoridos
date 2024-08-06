f99edaebd04a75fd85a662fcaf24ea1dcc127a33
HeitorGonzalez <heitor.gonzales.oliveira@escola.pr.gov.br>
2024-08-06 11:38:29 -0300

    A

---
+++ sketch.js
@@ -0,0 +1,44 @@
+let cor; 
+let x; // x
+let y; // y
+
+
+
+
+function setup() {
+random (0, 255);
+  createCanvas(400, 400);
+background ("white")
+cor = color(random(0, 255), random(0, 255), random(0, 255));
+x = 200;
+y = 200;
+}
+
+function draw() {
+fill (cor);
+                 circle(x, y, 50)
+
+if(mouseX < x) {
+x = x - 1;
+}
+if (mouseX > x) {
+x = x + 1;
+}
+
+if(mouseY < y) {
+y = y -1;
+}
+if(mouseY > y) {
+y = y + 1
+}
+
+if(mouseIsPressed) {
+
+cor = color(random(0, 255), random(0, 255), random(0, 255),
+random(0, 100));
+}
+
+
+
+
+}
